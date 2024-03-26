270ad1e37f6221b28c50869ce6e7757a3f4e8f54
S.Aravind <s.aravindmdofinbo2006@gmail.com>
2024-03-26 10:56:46 +0530

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,9 +1,3 @@
 export default {
-    apiKey: "AIzaSyCq9tatfm2mbjVhosIBYOFhSZZLKPmpMxE",
-    authDomain: "ecodrive-iitm.firebaseapp.com",
-    projectId: "ecodrive-iitm",
-    storageBucket: "ecodrive-iitm.appspot.com",
-    messagingSenderId: "673374441321",
-    appId: "1:673374441321:web:0c9f3b836c6d675a85f7f8",
-    measurementId: "G-B70LGW8M2H"
-}
+//put your firebase config here
+    }
